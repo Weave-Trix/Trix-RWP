da1cca93f32782f63336f221ef66770ae10f78ea
Weave-Trix <88459877+Weave-Trix@users.noreply.github.com>
2022-03-01 08:08:39 +0800

    heroku successful

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 const BASE_URL = "http://localhost:3001/api/"
+const HEROKU_URL ="https://secret-bayou-75630.herokuapp.com/api/"
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
-    baseURL: BASE_URL,
+    //baseURL: BASE_URL,
+    baseURL: HEROKU_URL,
 });
 
 export const userRequest = axios.create({
-    baseURL: BASE_URL,
+    //baseURL: BASE_URL,
+    baseURL: HEROKU_URL,
     header: { token: `Bearer ${TOKEN}` },
 })
